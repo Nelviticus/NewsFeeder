42e55e83deb3523fd74a1fd33b08d836f9bca8fe
sʍǝɥʇʇɐW lᴉǝN <neil.matthews@hymans.co.uk>
2019-09-24 11:10:31 +0100

    Ready to test publish

--- NewsFeeder.Repositories/EmpireNewsRepository.cs
+++ NewsFeeder.Repositories/EmpireNewsRepository.cs
@@ -8,48 +8,48 @@
     using System.Text;
     using Newtonsoft.Json;
 
-    public class Source
-    {
-        public string altText { get; set; }
-        public string src { get; set; }
-        public int media { get; set; }
-    }
-
-    public class Category
-    {
-        public string name { get; set; }
-        public string url { get; set; }
-    }
-
-    public class Item
-    {
-        public string id { get; set; }
-        public string title { get; set; }
-        public string description { get; set; }
-        public string date { get; set; }
-        public object rating { get; set; }
-        public List<Source> sources { get; set; }
-        public string icon { get; set; }
-        public string url { get; set; }
-        public Category category { get; set; }
-    }
-
-    public class Data
-    {
-        public List<Item> items { get; set; }
-        public string type { get; set; }
-        public string componentId { get; set; }
-    }
-
-    public class NewsItemChunk
-    {
-        public string id { get; set; }
-        public string name { get; set; }
-        public Data data { get; set; }
-    }
-
     public class EmpireNewsRepository: INewsRepository
     {
+        private struct Source
+        {
+            public string AltText { get; set; }
+            public string Src { get; set; }
+            public int Media { get; set; }
+        }
+
+        private struct Category
+        {
+            public string Name { get; set; }
+            public string Url { get; set; }
+        }
+
+        private struct Item
+        {
+            public string Id { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public string Date { get; set; }
+            public object Rating { get; set; }
+            public List<Source> Sources { get; set; }
+            public string Icon { get; set; }
+            public string Url { get; set; }
+            public Category Category { get; set; }
+        }
+
+        private struct Data
+        {
+            public List<Item> Items { get; set; }
+            public string Type { get; set; }
+            public string ComponentId { get; set; }
+        }
+
+        private struct NewsItemChunk
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+            public Data Data { get; set; }
+        }
+
         public string Title => "Empire Latest Movie News";
         public string SourceLink => "https://www.empireonline.com/movies/news/";
         public string Description => "The latest movie news from Empire magazine";
@@ -95,126 +95,31 @@
                     || !nodeText.Contains('}'))
                     continue;
 
-                try
-                {
-                    string nodeJson = nodeText.Substring(nodeText.IndexOf("{", StringComparison.Ordinal),
-                        nodeText.LastIndexOf("}", StringComparison.Ordinal) -
-                        nodeText.IndexOf("{", StringComparison.Ordinal) + 1);
-                    NewsItemChunk newsItemChunk = JsonConvert.DeserializeObject<NewsItemChunk>(nodeJson);
-
-                    foreach (Item item in newsItemChunk.data.items)
-                    {
-                        NewsArticle article = new NewsArticle();
-                        article.Title = item.title;
-                        article.Link = $"{_empireUrl}{item.url}";
-                        article.Description = GetArticleDescription($"{_empireUrl}{item.url}");
-                        string[] dateElements = item.date.Split(' ');
-                        DateTime pubDate = DateTime.UtcNow;
-                        try
-                        {
-                            switch (dateElements[1])
-                            {
-                                case "hour":
-                                    pubDate = pubDate.AddHours(-1);
-                                    break;
-                                case "hours":
-                                    pubDate = pubDate.AddHours(0d - double.Parse(dateElements[0]));
-                                    break;
-                                case "day":
-                                    pubDate = pubDate.AddDays(-1);
-                                    break;
-                                case "days":
-                                    pubDate = pubDate.AddDays(0d - double.Parse(dateElements[0]));
-                                    break;
-                            }
-                        }
-                        finally
-                        {
-                            article.PublicationDate = pubDate.AddMinutes(-_newsArticles.Count);
-                        }
-                        article.Guid = item.id;
-                        string imgSrc = item.sources.First().src.Replace("width=750", "width=150");
-                        article.ImageSrc = $"https:{imgSrc}";
-
-                        _newsArticles.Add(article);
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                    continue;
-                }
-            }
-
-            return _newsArticles;
-
-            HtmlNodeCollection cardNodeList = newsDocument.DocumentNode.SelectNodes("//div[contains(concat(' ', normalize-space(@class), ' '), ' card ')]");
-
-            if (cardNodeList == null)
-            {
-                return _newsArticles;
-            }
-
-            foreach (HtmlNode cardNode in cardNodeList)
-            {
-                AddNewsArticle(cardNode);
+                AddNodeArticles(nodeText);
             }
 
             return _newsArticles;
         }
 
-        private void AddNewsArticle(HtmlNode cardNode)
+        private void AddNodeArticles(string nodeText)
         {
-            IEnumerable<HtmlNode> headerNodes = cardNode.Descendants("h3").ToList();
-            IEnumerable<HtmlNode> paragraphNodes = cardNode.Descendants("p").ToList();
-            IEnumerable<HtmlNode> linkNodes = cardNode.Descendants("a").ToList();
-            IEnumerable<HtmlNode> imageNodes = cardNode.Descendants("img").ToList();
-            IEnumerable<HtmlNode> spanNodes = cardNode.Descendants("span").ToList();
+            string nodeJson = nodeText.Substring(nodeText.IndexOf("{", StringComparison.Ordinal),
+                nodeText.LastIndexOf("}", StringComparison.Ordinal) -
+                nodeText.IndexOf("{", StringComparison.Ordinal) + 1);
+            NewsItemChunk newsItemChunk = JsonConvert.DeserializeObject<NewsItemChunk>(nodeJson);
 
-            NewsArticle article = new NewsArticle();
-
-            if (headerNodes.Any())
+            foreach (Item item in newsItemChunk.Data.Items)
             {
-                article.Title = headerNodes.First().InnerText;
-            }
-
-            if (linkNodes.Any())
-            {
-                string linkHref = linkNodes.First().GetAttributeValue("href", string.Empty);
-                article.Guid = linkHref;
-                if (!linkHref.StartsWith("http"))
+                NewsArticle article = new NewsArticle();
+                article.Title = System.Web.HttpUtility.HtmlEncode(item.Title);
+                article.Link = $"{_empireUrl}{item.Url}";
+                article.Description = GetArticleDescription($"{_empireUrl}{item.Url}");
+                if (article.Description == string.Empty)
                 {
-                    article.Link = $"{_empireUrl}{linkHref}";
-                }
-                else
-                {
-                    article.Link = linkHref;
+                    article.Description = item.Description;
                 }
 
-                article.Description = GetArticleDescription(linkHref.StartsWith("http") ? linkHref : $"{_empireUrl}{linkHref}");
-            }
-
-            if (article.Description == string.Empty && paragraphNodes.Any())
-            {
-                article.Description = paragraphNodes.First().InnerText;
-            }
-
-            if (imageNodes.Any())
-            {
-                string imgSrc = imageNodes.First().GetAttributeValue("src", string.Empty).Replace("width=750", "width=150");
-                if (!imgSrc.StartsWith("http"))
-                {
-                    article.ImageSrc = $"http:{imgSrc}";
-                }
-                else
-                {
-                    article.ImageSrc = imgSrc;
-                }
-            }
-
-            if (spanNodes.Any())
-            {
-                string[] dateElements = spanNodes.Last().InnerText.Split(' ');
+                string[] dateElements = item.Date.Split(' ');
                 DateTime pubDate = DateTime.UtcNow;
                 try
                 {
@@ -238,9 +143,12 @@
                 {
                     article.PublicationDate = pubDate.AddMinutes(-_newsArticles.Count);
                 }
-            }
 
-            _newsArticles.Add(article);
+                article.Guid = item.Id;
+                article.ImageSrc = $"https:{item.Sources.Last().Src}";
+
+                _newsArticles.Add(article);
+            }
         }
 
         private string GetArticleDescription(string articleLink)
@@ -248,6 +156,9 @@
             HtmlWeb articleWeb = new HtmlWeb();
             HtmlDocument articleDocument = articleWeb.Load(articleLink);
             HtmlNode contentNode = articleDocument.DocumentNode.SelectSingleNode("//div[contains(concat(' ', normalize-space(@class), ' '), ' article__content ')]");
+            if (contentNode == null)
+                return string.Empty;
+
             IEnumerable<HtmlNode> paragraphNodes = contentNode.Descendants("p");
             StringBuilder descriptionBuilder = new StringBuilder();
             foreach (HtmlNode paragraphNode in paragraphNodes)
