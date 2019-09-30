231f54f08ebc94ac1ed5a35f35b28340d14556dc
sʍǝɥʇʇɐW lᴉǝN <neil.matthews@hymans.co.uk>
2019-09-30 11:09:09 +0100

    Working now using embedded JSON

--- NewsFeeder.Repositories/EmpireNewsRepository.cs
+++ NewsFeeder.Repositories/EmpireNewsRepository.cs
@@ -14,6 +14,9 @@
         public string Title => "Empire Latest Movie News";
         public string SourceLink => "https://www.empireonline.com/movies/news/";
         public string Description => "The latest movie news from Empire magazine";
+        private string _empireUrl = "https://www.empireonline.com";
+        private readonly List<INewsArticle> _newsArticles = new List<INewsArticle>();
+        private readonly int _desiredImageWidth = 300;
 
         public string Body
         {
@@ -32,9 +35,6 @@
                 return feedBuilder.ToString();
             }
         }
-
-        private string _empireUrl = "https://www.empireonline.com";
-        private readonly List<INewsArticle> _newsArticles = new List<INewsArticle>();
 
         public IEnumerable<INewsArticle> NewsArticles()
         {
@@ -76,43 +76,60 @@
                     Guid = item.Id,
                     Title = System.Web.HttpUtility.HtmlEncode(item.Title),
                     Link = $"{_empireUrl}{item.Url}",
-                    Description = GetArticleDescription($"{_empireUrl}{item.Url}")
+                    Description = GetArticleDescription($"{_empireUrl}{item.Url}"),
+                    PublicationDate = GetPublicationDate(item.Date),
+                    ImageSrc = GetImageSource(item.Sources)
                 };
                 if (article.Description == string.Empty)
                 {
                     article.Description = item.Description;
                 }
 
-                string[] dateElements = item.Date.Split(' ');
-                DateTime pubDate = DateTime.UtcNow;
-                try
-                {
-                    switch (dateElements[1])
-                    {
-                        case "hour":
-                            pubDate = pubDate.AddHours(-1);
-                            break;
-                        case "hours":
-                            pubDate = pubDate.AddHours(0d - double.Parse(dateElements[0]));
-                            break;
-                        case "day":
-                            pubDate = pubDate.AddDays(-1);
-                            break;
-                        case "days":
-                            pubDate = pubDate.AddDays(0d - double.Parse(dateElements[0]));
-                            break;
-                    }
-                }
-                finally
-                {
-                    article.PublicationDate = pubDate.AddMinutes(-_newsArticles.Count);
-                }
-
-                if (item.Sources.Any())
-                    article.ImageSrc = $"https:{item.Sources.Last().Src}";
-
                 _newsArticles.Add(article);
             }
+        }
+
+        private string GetImageSource(List<Source> itemSources)
+        {
+            if (!itemSources.Any()) return string.Empty;
+
+            string itemImageSource = itemSources.First().Src;
+            int widthElementPosition = itemImageSource.LastIndexOf("&width=", StringComparison.OrdinalIgnoreCase);
+            if (widthElementPosition >= 0)
+            {
+                itemImageSource = itemImageSource.Substring(0, widthElementPosition) + "&width=" + _desiredImageWidth;
+            }
+            return $"https:{itemImageSource}";
+        }
+
+        private DateTime GetPublicationDate(string itemDate)
+        {
+            string[] dateElements = itemDate.Split(' ');
+            DateTime pubDate = DateTime.UtcNow;
+            try
+            {
+                switch (dateElements[1])
+                {
+                    case "hour":
+                        pubDate = pubDate.AddHours(-1);
+                        break;
+                    case "hours":
+                        pubDate = pubDate.AddHours(0d - double.Parse(dateElements[0]));
+                        break;
+                    case "day":
+                        pubDate = pubDate.AddDays(-1);
+                        break;
+                    case "days":
+                        pubDate = pubDate.AddDays(0d - double.Parse(dateElements[0]));
+                        break;
+                }
+            }
+            finally
+            {
+                pubDate = pubDate.AddMinutes(-_newsArticles.Count);
+            }
+
+            return pubDate;
         }
 
         private string GetArticleDescription(string articleLink)
